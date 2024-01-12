7422a2fa4ae12f22ff4b21cef9ba8f48dd9e0ad3
essram <esarama22@gmail.com>
2024-01-12 10:22:16 +0700

    first commit

---
+++ asdrone1.java
@@ -0,0 +1,14 @@
+package materi.OOP.drone;
+
+public class asdrone1 {
+    public static void main(String[] args) {
+        Drone drone = new Drone();
+
+        drone.energi = 10;
+        drone.ketinggian = 200;
+        drone.kecepatan = 180;
+
+        drone.terbang();
+
+    }
+}
---
+++ asdrone2.java
@@ -0,0 +1,35 @@
+package materi.OOP.drone;
+
+
+class drone{
+    int energi;
+    int ketinggian;
+    int kecepatan;
+    String merk;
+
+    void terbang(){
+        energi--;
+        if(energi > 10){
+
+            ketinggian++;
+            System.out.println("drone terbang");
+        } else {
+            System.out.println("Energi lemah (Drone Tidak Bisa Terbsng)");
+        }
+    }
+    void matikanMesin(){
+        if(ketinggian > 0) {
+        System.out.println("Mesin tidak bisa dimatikan karena sedang terbang");
+    } else {
+        System.out.println("Mesin dimatikan");
+    }
+}
+
+
+void turun(){
+
+    ketinggian--;
+    energi--;
+    System.out.println("Drone turun");
+}
+}
